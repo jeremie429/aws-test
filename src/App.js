e8d4c7c2943b9ead4cb2a21c1488c508c7400201
jeremie429 <jeremietchapnda2011@yahoo.fr>
2022-09-08 09:53:29 +0100

    added amplify auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+// amplify stuff
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">HELLO WORLD</header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
